6894adfef9f2bd776a5c47516f92b5c3775e4125
Mariana Marques <ma_oliveiramarques@hotmail.com>
2022-09-19 00:52:51 +0100

    palindrome recursive function
    added argument to the function

--- palindrome.js
+++ palindrome.js
@@ -1,23 +1,25 @@
 // Check if a word is a palindrome by a recursive function
-
-function checkPalindrome(word) {
+function checkPalindrome(wordToPrint, word) {
   let str = word.toLowerCase()
   let wordStart = 0
   let wordEnd = str.length - 1
 
   // caso base: Se a palavra tem 1 ou 2 letras, é um palíndrome
   if (str.length <= 1) {
-    return `Yes! ${word} it's a palíndrome!`
+    return `Yes! ${wordToPrint} is a palíndrome!`
   }
   // Se as letras no início e no fim de palavra são diferentes, não é um palíndrome
   if (str.charAt(wordStart) !== str.charAt(wordEnd)) {
-    return `I'm sorry! ${word} it's not a palíndrome!`
+    return `I'm sorry! ${wordToPrint} is not a palíndrome!`
   }
   // Se as letras são iguais, cortar as letras no início e no fim e verificar a nova palavra (subword)
   let subword = word.slice(1, wordEnd)
-  return checkPalindrome(subword)
+  return checkPalindrome(wordToPrint, subword)
 }
 
-console.log(checkPalindrome('ABBA'))
-console.log(checkPalindrome('Sapo'))
-console.log(checkPalindrome('Arara'))
+const abbaWord = 'ABBA'
+console.log(checkPalindrome(abbaWord, abbaWord))
+const sapoWord = 'Sapo'
+console.log(checkPalindrome(sapoWord, sapoWord))
+const araraWord = 'Arara'
+console.log(checkPalindrome(araraWord, araraWord))
