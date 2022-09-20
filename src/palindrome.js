ca99736e254b733ce22f3f9fe7ac4b0188c4fe69
Pedro Soares <pncsoares@gmail.com>
2022-09-20 14:02:03 +0100

    Create palindrome check code

---
+++ src/palindrome.js
@@ -0,0 +1,46 @@
+function checkPalindrome(word) {
+    const itIsPalindrome = isPalindrome(word);
+    return getMessage(word, itIsPalindrome);
+}
+
+function isPalindrome(word) {
+    if (word.length < 2) {
+        return true;
+    }
+
+    const lowerCaseWord = convertToLowerCase(word);
+
+    const firstCharacter = getFirstCharacter(lowerCaseWord);
+    const lastCharacter = getLastCharacter(lowerCaseWord);
+
+    if (firstCharacter == lastCharacter) {
+        const middleCharacters = getMiddleCharacters(lowerCaseWord);
+        return isPalindrome(middleCharacters);
+    }
+
+    return false;
+}
+
+function getMessage(word, itIsPalindrome) {
+    return `${word} ${itIsPalindrome ? 'is' : 'is not'} a palindrome`;
+}
+
+function convertToLowerCase(string) {
+    return string.toLowerCase();
+}
+
+function getFirstCharacter(string) {
+    return string.charAt(0);
+}
+
+function getMiddleCharacters(string) {
+    return string.slice(1, -1);
+}
+
+function getLastCharacter(string) {
+    return string.slice(-1);
+}
+
+console.log(checkPalindrome('ABBA'));
+console.log(checkPalindrome('Sapo'));
+console.log(checkPalindrome('Arara'));
